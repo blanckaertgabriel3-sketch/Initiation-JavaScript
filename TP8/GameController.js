00dca5b149a234f524d46506953d52b60fb1b0d5
gabriel <blanckaertgabriel@Gabriel>
2026-01-20 15:29:35 +0100

    commencement partie6

--- TP8/GameController.js
+++ TP8/GameController.js
@@ -17,18 +17,28 @@ class GameController {
         this.pseudo = localStorage.getItem("pseudo");
         this.serverUrl = "ws://localhost:8000/ws";
         // serverUrl = localStorage.getItem("serverUrl");
-        this.selectedSkin = localStorage.getItem("skin");
-
+        this.skinPath = localStorage.getItem("skin");
+        
         //objet permetant de gérer les événements
-        this.action = {
+        this.inputState = {
             up: false,
             down: false,
             left: false,
             right: false,
             attack: false,
-        }
+        };
+        
+        //URL Serveur
+        this.socket = new WebSocket(this.serverUrl);
+        
+        this.initSocket();
+        this.initInput();
+        this.startInputSender();
+    }
+
+    initSocket(){
+        console.log("initSocket");
         //déclaration de l'attribut webSocket
-        this.socket = new WebSocket("ws://localhost:8000/ws");
         this.socket.onopen = () => {
             console.log("connecté !");  
             this.socket.send(JSON.stringify({
@@ -36,18 +46,85 @@ class GameController {
                 skinPath: this.skinPath
             }));    
         };     
-
-
+    
+        //réception de message du backend
+        this.socket.onmessage = function(event) {
+            //--------------------- console.log("message du backend !", event.data);
+        }
+    }
+    //mettre 2 écouteurs keyup et keydown sur les touches
+    initInput() {
+        this.nomTouche = "";
+        //écouteur pour pression de touche 
+        document.addEventListener("keydown", (event) => {
+            this.nomTouche = event.key;
+            //événement haut
+            if (this.nomTouche == "z") {
+                this.inputState.up = true;
+            }
+            //événement bas
+            else if (this.nomTouche == "s") {
+                this.inputState.down = true;
+            }
+            //événement gauche
+            else if (this.nomTouche == "q") {
+                this.inputState.left = true;
+            }
+            //événement droite
+            else if (this.nomTouche == "d") {
+                this.inputState.right = true;
+            }
+            //événement attaque
+            else if (this.nomTouche == " ") {
+                this.inputState.attack = true;
+            }
+            if (this.nomTouche) {
+                console.log("pression",this.inputState);
+            }
+        });
         
-        this.initSocket();
+        //écouteur pour relachement de touche 
+        document.addEventListener("keyup", (event) => {
+            this.nomTouche = event.key;
+            //événement haut
+            if (this.nomTouche == "z") {
+                this.inputState.up = false;
+            }
+            //événement bas
+            else if (this.nomTouche == "s") {
+                this.inputState.down = false;
+            }
+            //événement gauche
+            else if (this.nomTouche == "q") {
+                this.inputState.left = false;
+            }
+            //événement droite
+            else if (this.nomTouche == "d") {
+                this.inputState.right = false;
+            }
+            //événement attaque
+            else if (this.nomTouche == " ") {
+                this.inputState.attack = false;
+            }
+            if (this.nomTouche) {
+                console.log("relache",this.inputState);
+            }
+        })
+        
     }
-    initSocket(){
-        console.log("initSocket");
-    }
+    //Envoi de messages au backend
+    startInputSender() {
 
+    }
+    //afin d’appeler un callback toutes les this.SERVER_INTERVAL millisecondes.
+    setInterval() {
+
+    }
+    
+    
     // === Main render loop ===
     loop(timestamp) {
-
+        
         // Request the next frame
         requestAnimationFrame(this.loop);
     }
